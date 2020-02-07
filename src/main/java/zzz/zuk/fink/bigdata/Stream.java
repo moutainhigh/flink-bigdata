4852e848aeda85debf202539b8881a25412b5c9a
huangliao <huangliao@192.168.0.104>
2020-02-07 22:37:09 +0800

    提交代码

---
+++ src/main/java/zzz/zuk/fink/bigdata/Stream.java
@@ -0,0 +1,17 @@
+package zzz.zuk.fink.bigdata;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Stream {
+
+	public static void main(String[] args) {
+		
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+		
+	
+		
+	}
+	
+	
+	
+}
